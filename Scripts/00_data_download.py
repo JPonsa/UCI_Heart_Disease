8d4a0352785da4890a71211df933efa6d0e15a0f
Joan Ponsa <joan.ponsa.cobas@gmail.com>
2023-10-30 08:30:34 +0000

    first commit

---
+++ Scripts/00_data_download.py
@@ -0,0 +1,88 @@
+################################################################################################
+# Author: Joan Ponsa
+# Created on: 29/10/2023
+# Last modified: 29/10/2023
+#
+# Downloads the original UCI heart diseases dataset and joins the data in a single file
+#
+################################################################################################
+
+# Import libraries
+from dotenv import load_dotenv
+import numpy as np
+import os
+import pandas as pd
+
+# Set Working directory
+load_dotenv()
+WORKING_DIR = os.getenv("WORKING_DIR")
+os.chdir(WORKING_DIR)
+
+# Create Data directory if doesn't exist
+if not os.path.isdir(WORKING_DIR + "/Data/"):
+    os.mkdir(WORKING_DIR + "/Data/")
+
+# Create Origila_dataset directory if doesn't exist
+if not os.path.isdir(WORKING_DIR + "/Data/Original_dataset/"):
+    os.mkdir(WORKING_DIR + "/Data/Original_dataset/")
+    print(WORKING_DIR + "/Data/Original_dataset/ created")
+
+# Download data from UCI
+if ~os.path.isfile(WORKING_DIR + "/Data/Original_dataset/heart+disease.zip"):
+    # Download file
+    import urllib.request
+
+    url = "https://archive.ics.uci.edu/static/public/45/heart+disease.zip"
+    path_to_zip_file = WORKING_DIR + "/Data/Original_dataset/heart+disease.zip"
+    urllib.request.urlretrieve(url, path_to_zip_file)
+
+    # Unizip file
+    import zipfile
+
+    with zipfile.ZipFile(path_to_zip_file, "r") as zip_ref:
+        zip_ref.extractall(WORKING_DIR + "/Data/Original_dataset/")
+
+# table field names from original dataset
+column_names = [
+    "age",
+    "sex",
+    "cp",
+    "trestbps",
+    "chol",
+    "fbs",
+    "restecg",
+    "thalach",
+    "exang",
+    "oldpeak",
+    "slope",
+    "ca",
+    "thal",
+    "num",
+]
+
+# Data was collected in from 4 locations: Cleveland, Hungary, Switzerland and Long Beach VA
+# See Data/Original_dataset/heart-diease.names
+
+data_centers = ["cleveland", "hungarian", "switzerland", "va"]
+
+# Combine all 4 datasets in a single file
+df = pd.DataFrame([], columns=["data_center"] + column_names)
+
+for center in data_centers:
+    # Read the original dataset
+    tmp = pd.read_csv(f"Data/Original_dataset/processed.{center}.data", header=None)
+    # Add the column names
+    tmp.columns = column_names
+    # Add a column with the data center name (source of the data)
+    tmp.insert(loc=0, column="data_center", value=center)
+    # Concatenate all in a single final file
+    df = pd.concat([df, tmp])
+
+# missing values are encoded using "?". Replace them with NaN
+df.replace("?", np.nan, inplace=True)
+
+# Save concatenated data in a single file for further processing - No data changes
+output_file = (
+    WORKING_DIR + "/Data/uci_heart_disease.original_processed.four_databases.tsv"
+)
+df.to_csv(output_file, sep="\t", index=False)
---
+++ Scripts/10_data_processing.py
@@ -0,0 +1,93 @@
+################################################################################################
+# Author: Joan Ponsa
+# Created on: 28/10/2023
+# Last modified: 29/10/2023
+#
+# Takes the original UCI heart disease dataset and does any data transformation prior ML
+#
+################################################################################################
+
+# Import libraries
+from dotenv import load_dotenv
+import numpy as np
+import os
+import pandas as pd
+
+# Set Working directory
+load_dotenv()
+WORKING_DIR = os.getenv("WORKING_DIR")
+os.chdir(WORKING_DIR)
+
+# Load original dataset
+# fmt: off
+input_file = WORKING_DIR + "Data/uci_heart_disease.original_processed.four_databases.tsv"
+# fmt: on
+df = pd.read_csv(input_file, sep="\t")
+
+# Create a binary version of variable num
+df["num_01"] = df["num"].apply(lambda x: 1 if x > 0 else 0)
+
+# Data correction
+# cholesterol values > 400 are outliers, replacing them with 401
+df["chol"].mask((df["chol"].notna() & df["chol"] > 400), 401, inplace=True)
+# values of 0 cholesterol make no sense. I will assume any 0 value is a missing value.
+df["chol"].replace(0, np.nan, inplace=True)
+
+# values of 0 trestbps make no sense. I will assume any 0 value is a missing value.
+df["trestbps"].replace(0, np.nan, inplace=True)
+# I believe negative "oldpeak" make no sense. Replacing any negative value with NaN
+df["oldpeak"].mask(df["oldpeak"] < 0, np.nan, inplace=True)
+
+# Recode variables
+# cp: 1 = typica angina, 2= atypical angina, 3=non-aginal pain, 4= asymptomatic
+df["cp_1"] = 0
+df.loc[df["cp"] == 1, "cp_1"] = 1
+df["cp_2"] = 0
+df.loc[df["cp"] == 2, "cp_2"] = 1
+df["cp_3"] = 0
+df.loc[df["cp"] == 3, "cp_3"] = 1
+
+# restecg 0 = normal, 1 = abnormality, 2 = left ventricular hypertrophy
+df["restecg_1"] = 0
+df.loc[df["restecg"] == 1, "restecg_1"] = 1
+df["restecg_2"] = 0
+df.loc[df["restecg"] == 2, "restecg_2"] = 1
+
+# slope: 1 = upsloping, 2 =  flat, 3 = downslopling
+df["slope"].replace({1: 1, 2: 0, 3: -1}, inplace=True)
+
+# thal: 3 = normal; 6 = fixed defect; 7 = reversable defect
+df["thal_6"] = 0
+df.loc[df["thal"] == 6, "thal_6"] = 1
+df["thal_7"] = 0
+df.loc[df["thal"] == 7, "thal_7"] = 1
+
+# TODO: Imputation of missing values
+
+# Remove the VA data center due to poor data quality
+df = df[df["data_center"] != "va"]
+
+
+# Feature manually selected based on the low number of missing values
+feature_subset = [
+    "age",
+    "sex",
+    "cp_1",
+    "cp_2",
+    "cp_3",
+    "trestbps",
+    "restecg_1",
+    "restecg_2",
+    "thalach",
+    "exang",
+    "oldpeak",
+    "num_01",
+]
+# Trim feature to only the selected subset
+df = df[feature_subset]
+# remove any values with missing records
+df.dropna(inplace=True)
+
+# Save preprocessed data
+output_file = WORKING_DIR + "Data/uci_heart_disease.processed.tsv"
+df.to_csv(output_file, sep="\t", index=False)
